80197dfad54c76601878e513fce892428cbe78b5
Son Pham-Ba <son.phamba@epfl.ch>
2026-05-26 17:02:53 +0200

    feat: change undamaged color from cyan to black

--- frontend/src/stores/annotation-data.ts
+++ frontend/src/stores/annotation-data.ts
@@ -16,7 +16,7 @@ import { Notify } from 'quasar';
 import { getI18nT } from 'src/utils/i18n';
 
 export const DAMAGE_LEVELS: DamageLevelType[] = ['unset', 'undamaged', 'damaged'];
-export const DAMAGE_COLORS = ['#00e8d2', '#1974d2', '#ff007f'];
+export const DAMAGE_COLORS = ['#444444', '#1974d2', '#ff007f'];
 
 const OVERLAP_RATIO_THRESHOLD = 0.3;
 
